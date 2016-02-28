707950cf394bb0897f6800ca44a795687361221a
myroslav <theowgp@gmail.com>
2016-02-28 14:02:56 +0100

    u only for the leader

--- fv.m
+++ fv.m
@@ -1,5 +1,6 @@
 function res = fv(k, v, solx, u, N, d)% fx:R*Rd(N+1)*Rd(N+1) -> Rd(N+1)
 
-res = S(v, N, d) + M(solx(:, :, k), N, d) + E(solx(:, :, k), v, N, d) + L(solx(:, :, k), N, d) + u(N+2:2*(N+1), :, k);
+res = S(v, N, d) + M(solx(:, :, k), N, d) + E(solx(:, :, k), v, N, d) + L(solx(:, :, k), N, d);
+res(1, :) = res(1, :) + u(:, k)';
 
 end
